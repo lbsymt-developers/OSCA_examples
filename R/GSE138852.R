c90392768db0f8042e4ed269c7be6348edbb2403
Erickcufe <erickcufe94@gmail.com>
2022-03-09 16:47:05 -0600

    añadiendo metadata al dataset gse138852

--- R/GSE138852.R
+++ R/GSE138852.R
@@ -32,3 +32,18 @@ saveRDS(sparse.mat, "data/GSE138852_sparseMatrix.rds")
 
 # sparse.mat <- readSparseCounts(data.table::fread("GSE138852_counts.csv.gz"))
 # Este no se pudo, hay que indagar la causa
+
+
+# Creamos el objeto singlecellexperiment
+library(SingleCellExperiment)
+mat <- readRDS("data/GSE138852_sparseMatrix.rds")
+sce_138852 <- SingleCellExperiment::SingleCellExperiment(assays = list(counts = mat))
+
+assay(sce_138852, "counts")
+sce_138852 <- scuttle::logNormCounts(sce_138852)
+sce_138852
+assay(sce_138852, "logcounts")
+
+meta <- readr::read_delim("data/scRNA_metadata.tsv")
+
+
